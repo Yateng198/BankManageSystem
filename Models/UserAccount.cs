266d51427409681782f03366614ce79119f584b4
Yateng Geng <yatenggeng@gmail.com>
2023-02-20 15:39:57 -0500

    Update Register

---
+++ Models/UserAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManageSystem.Models
+{
+    internal class UserAccount
+    {
+        private int userId { get; set; }
+        private string cardNumber { get; set; }
+        private float balance { get; set; }
+    }
+}
--- Register.xaml.cs
+++ Register.xaml.cs
@@ -22,11 +22,19 @@ namespace BankManageSystem
     public partial class Register : Window
     {
         SqlConnection con;
+        DateTime MinimumDate = new DateTime(1900, 1, 1);
+        DateTime MaximumDate = DateTime.Today.AddYears(-18);
+
+
         public Register()
         {
             InitializeComponent();
-            con = new SqlConnection("Data Source=LAPTOP-DT6BMRBG;Initial Catalog=final;Integrated Security=True");
+            con = new SqlConnection("Data Source=DESKTOP-1AHTENP\\MSSQLSERVER01;Initial Catalog=BankManageSystemNewDB;Integrated Security=True");
             con.Open();
+
+            dobPicker.DisplayDateStart = MinimumDate;
+            dobPicker.DisplayDateEnd = MaximumDate;
+
 
         }
 
@@ -34,20 +42,35 @@ namespace BankManageSystem
         {
             try
             {
-               // DatePicker dp = new DatePicker();
-                //Open the db connection
-                string qury = "insert into customerTable values(CAST((RAND()*(9832789-3)+(RAND()*(89732-298)))*100 AS BIGINT),@FName, @LName, @Dob, @Country, @PhoneNumber, @Email, @Password)";
+                string qury = "INSERT INTO UserInfo OUTPUT INSERTED.UserId values(@Password, @FName, @LName, @Dob, @Email, @PhoneNumber, @occupation, @street, @city, @province, @country, @postalcode)";
                 SqlCommand cmd = new SqlCommand(qury, con);
                 cmd.Parameters.AddWithValue("@FName", fname.Text);//We need to call the textbox by name to grab the text in it
                 cmd.Parameters.AddWithValue("@LName", lname.Text);
-                cmd.Parameters.AddWithValue("@Dob", dobPicker.ToString());
+                cmd.Parameters.AddWithValue("@Dob", dobPicker.SelectedDate.Value.Date);
                 cmd.Parameters.AddWithValue("@Country", country.Text);
-                cmd.Parameters.AddWithValue("@PhoneNumber", phonenumber.Text);
+                cmd.Parameters.AddWithValue("@PhoneNumber", mobile.Text);
                 cmd.Parameters.AddWithValue("@Email", email.Text);
                 cmd.Parameters.AddWithValue("@Password", pwd.Password);
+                cmd.Parameters.AddWithValue("@occupation", occupation.Text);
+                cmd.Parameters.AddWithValue("@street", street.Text);
+                cmd.Parameters.AddWithValue("@city", city.Text);
+                cmd.Parameters.AddWithValue("@province", province.Text);
+                cmd.Parameters.AddWithValue("@postalcode", postalcode.Text);
                 //we now need to excute our qury
                 cmd.ExecuteNonQuery();
+                //Generete the account number for this user
+                Random rand = new Random();
+                long accountNumber = (long)(rand.NextDouble() * (9832789 - 3) + rand.NextDouble() * (89732 - 298)) * 100;
+                //Retrieve the newly inserted user's id
+                int userId = (int)cmd.ExecuteScalar();
+                string accountQuery = "Insert Into UserAccount values (@UserId, @CardNumber, 0.0)";
+                SqlCommand accountCmd = new SqlCommand(accountQuery, con);
+                accountCmd.Parameters.AddWithValue("@UserId", userId);
+                accountCmd.Parameters.AddWithValue("@CardNumber", accountNumber);
+                accountCmd.ExecuteNonQuery();
+
                 MessageBox.Show("Registe Successfully, click ok go back to log in page!");
+
                 MainWindow mainWindow= new MainWindow();
                 mainWindow.email.Text = this.email.Text;
                 this.Close();
