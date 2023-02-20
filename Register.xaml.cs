6c39c35c04f4e39bac14b438f46f2bb269d78f87
Yateng Geng <yatenggeng@gmail.com>
2023-02-20 16:04:18 -0500

    Generating User Account number and Insert into DB while register

--- Register.xaml.cs
+++ Register.xaml.cs
@@ -42,40 +42,53 @@ namespace BankManageSystem
         {
             try
             {
-                string qury = "INSERT INTO UserInfo OUTPUT INSERTED.UserId values(@Password, @FName, @LName, @Dob, @Email, @PhoneNumber, @occupation, @street, @city, @province, @country, @postalcode)";
-                SqlCommand cmd = new SqlCommand(qury, con);
-                cmd.Parameters.AddWithValue("@FName", fname.Text);//We need to call the textbox by name to grab the text in it
-                cmd.Parameters.AddWithValue("@LName", lname.Text);
-                cmd.Parameters.AddWithValue("@Dob", dobPicker.SelectedDate.Value.Date);
-                cmd.Parameters.AddWithValue("@Country", country.Text);
-                cmd.Parameters.AddWithValue("@PhoneNumber", mobile.Text);
-                cmd.Parameters.AddWithValue("@Email", email.Text);
-                cmd.Parameters.AddWithValue("@Password", pwd.Password);
-                cmd.Parameters.AddWithValue("@occupation", occupation.Text);
-                cmd.Parameters.AddWithValue("@street", street.Text);
-                cmd.Parameters.AddWithValue("@city", city.Text);
-                cmd.Parameters.AddWithValue("@province", province.Text);
-                cmd.Parameters.AddWithValue("@postalcode", postalcode.Text);
-                //we now need to excute our qury
-                cmd.ExecuteNonQuery();
-                //Generete the account number for this user
-                Random rand = new Random();
-                long accountNumber = (long)(rand.NextDouble() * (9832789 - 3) + rand.NextDouble() * (89732 - 298)) * 100;
-                //Retrieve the newly inserted user's id
-                int userId = (int)cmd.ExecuteScalar();
-                string accountQuery = "Insert Into UserAccount values (@UserId, @CardNumber, 0.0)";
-                SqlCommand accountCmd = new SqlCommand(accountQuery, con);
-                accountCmd.Parameters.AddWithValue("@UserId", userId);
-                accountCmd.Parameters.AddWithValue("@CardNumber", accountNumber);
-                accountCmd.ExecuteNonQuery();
+                string newEmail = email.Text;
+                string selectQuery = "SELECT COUNT(*) FROM UserInfo WHERE Email = @Email";
+                SqlCommand selectCmd = new SqlCommand(selectQuery, con);
+                selectCmd.Parameters.AddWithValue("@Email", newEmail);
+                int count = (int)selectCmd.ExecuteScalar();
 
-                MessageBox.Show("Registe Successfully, click ok go back to log in page!");
+                if (count > 0)
+                {
+                    MessageBox.Show("Email already exists. Please enter a different email address.");
+                }
+                else
+                {
+                    string qury = "INSERT INTO UserInfo OUTPUT INSERTED.UserId values(@Password, @FName, @LName, @Dob, @Email, @PhoneNumber, @occupation, @street, @city, @province, @country, @postalcode)";
+                    SqlCommand cmd = new SqlCommand(qury, con);
+                    cmd.Parameters.AddWithValue("@FName", fname.Text);//We need to call the textbox by name to grab the text in it
+                    cmd.Parameters.AddWithValue("@LName", lname.Text);
+                    cmd.Parameters.AddWithValue("@Dob", dobPicker.SelectedDate.Value.Date);
+                    cmd.Parameters.AddWithValue("@Country", country.Text);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", mobile.Text);
+                    cmd.Parameters.AddWithValue("@Email", email.Text);
+                    cmd.Parameters.AddWithValue("@Password", pwd.Password);
+                    cmd.Parameters.AddWithValue("@occupation", occupation.Text);
+                    cmd.Parameters.AddWithValue("@street", street.Text);
+                    cmd.Parameters.AddWithValue("@city", city.Text);
+                    cmd.Parameters.AddWithValue("@province", province.Text);
+                    cmd.Parameters.AddWithValue("@postalcode", postalcode.Text);
+                    //we now need to excute our qury and Retrieve the newly inserted user's id
+                    int userId = (int)cmd.ExecuteScalar();
+                    //Generete the account number for this user
+                    Random rand = new Random();
+                    long accountNumber = (long)(rand.NextDouble() * (9832789 - 3) + rand.NextDouble() * (89732 - 298)) * 100;
 
-                MainWindow mainWindow= new MainWindow();
-                mainWindow.email.Text = this.email.Text;
-                this.Close();
-                mainWindow.Show();
-                con.Close();
+
+                    string accountQuery = "Insert Into UserAccount values (@UserId, @CardNumber, 0.0)";
+                    SqlCommand accountCmd = new SqlCommand(accountQuery, con);
+                    accountCmd.Parameters.AddWithValue("@UserId", userId);
+                    accountCmd.Parameters.AddWithValue("@CardNumber", accountNumber);
+                    accountCmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Registe Successfully, click ok go back to log in page!");
+
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.email.Text = this.email.Text;
+                    this.Close();
+                    mainWindow.Show();
+                    con.Close();
+                }
             }
             catch (SqlException ex)
             {
